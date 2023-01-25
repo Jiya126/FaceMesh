fa634e9da058f7cec2bfb74c6682517afae13dad
J <jiya.cm.20@nitj.ac.in>
2023-01-25 15:02:39 +0530

    Update face.py

--- face.py
+++ face.py
@@ -1,31 +1,53 @@
 import cv2
 import mediapipe as mp
 
-MpFaceMesh = mp.solutions.face_mesh
-faceMesh = MpFaceMesh.FaceMesh()
-draw = mp.solutions.drawing_utils
-drawSpec = draw.DrawingSpec(thickness=1, circle_radius=1)
+class FaceMeshDetector():
+    def __init__(self, staticMode, maxFaces, minDetectionConf, minTrackConf):
+        self.staticMode = staticMode
+        self.maxFaces = maxFaces
+        self.minDetectionConf = minDetectionConf
+        self.minTrackConf = minTrackConf
 
-cap = cv2.VideoCapture(0)
+        self.MpFaceMesh = mp.solutions.face_mesh
+        self.faceMesh = self.MpFaceMesh.FaceMesh()
+        self.mpDraw = mp.solutions.drawing_utils
+        self.drawSpec = self.mpDraw.DrawingSpec(thickness=1, circle_radius=1)
 
-while True:
-    s, img = cap.read()
-    imgC = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    out = faceMesh.process(imgC)
+    def findFaceMesh(self, img, draw):
+        self.imgC = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.out = self.faceMesh.process(self.imgC)
+        faceCoord = []
+        faces = []
     
-    if out.multi_face_landmarks:
-        for fLms in out.multi_face_landmarks:
-            draw.draw_landmarks(img, fLms, MpFaceMesh.FACEMESH_TESSELATION, drawSpec, drawSpec)
-        for id, lm in enumerate(fLms.landmark):
-            ih, iw, ic = img.shape
-            x,y = int(lm.x * iw), int(lm.y * ih)
-            print(id,x,y)
+        if self.out.multi_face_landmarks:
+            for fLms in self.out.multi_face_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(img, fLms, self.MpFaceMesh.FACEMESH_TESSELATION, self.drawSpec, self.drawSpec)
+            for id, lm in enumerate(fLms.landmark):
+                ih, iw, ic = img.shape
+                x,y = int(lm.x * iw), int(lm.y * ih)
+                # print(id,x,y)
+            faceCoord.append([x,y])
+            faces.append(faceCoord)
+
+        return faces, img
 
 
-    cv2.imshow('live', img)
+def main():
+    cap = cv2.VideoCapture(0)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    detector = FaceMeshDetector(staticMode=False, maxFaces=5, minDetectionConf=0.5, minTrackConf=0.5)
 
-cap.release()
-cv2.destroyAllWindows()
+    while True:
+        s, img = cap.read()
+        faces, img = detector.findFaceMesh(img, draw=True)
+        # print(faces)
+        cv2.imshow('live', img)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+main()
+
+    # cap.release()
+    # cv2.destroyAllWindows()
